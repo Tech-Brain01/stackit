09ea5b094ba89bbc89ca9da89ee91cd05690d528
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-07-12 10:26:42 +0530

    Created routes and setup app and main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-  return 
-  
-  <div>Hello World</div>;
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import AskQuestion from "./pages/AskQuestion";
+import AllQuestions from "./pages/ShowAllQuestions";
+import NotFound from "./pages/PageNotFound";
 
-}
+// Common Layout (optional)
+import Navbar from "./components/NavBar";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900">
+      <Navbar />
+
+      <main className="px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/ask" element={<AskQuestion />} />
+          <Route path="/questions" element={<AllQuestions />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/pages/ShowAllQuestions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowAllQuestions = () => {
+  return (
+    <div>ShowAllQuestions</div>
+  )
+}
+
+export default ShowAllQuestions
